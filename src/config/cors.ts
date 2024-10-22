9e2ed3aeb99dfa12de57c65e35102b176e5b0f26
jeyfredc <jeyfredc@gmail.com>
2024-10-22 18:22:21 -0500

    updated package.json and cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,18 +1,17 @@
-import { CorsOptions } from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
- 
+import { CorsOptions } from 'cors'
+
 export const corsConfig: CorsOptions = {
-  origin: function (origin, callback) {
-    const whiteList = [process.env.FRONTEND_URL];
- 
-    if (process.argv[2] == '--api') {
-      whiteList.push(undefined);
+    origin: function(origin, callback){
+        
+        const whitelist = [ process.env.FRONTEND_URL]        
+
+        if(process.argv[2] === '--api'){
+            whitelist.push(undefined)
+        }
+        if(whitelist.includes(origin)){
+            callback(null, true)
+        }else {
+            callback(new Error('Error de CORS'))
+        }
     }
-    if (whiteList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Error de CORS'));
-    }
-  },
-};
+}
